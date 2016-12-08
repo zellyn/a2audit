08074672cafe8abea353a9397a07bee01cd35c83
Zellyn Hunter <zellyn@squareup.com>
2016-12-07 23:05:07 -0500

    add first pass of machine detection to audit.asm
    Currently having issues with some plain Apple II and II+, but seems to
    work for IIe and IIe enhanced.

--- audit/audit.asm
+++ audit/audit.asm
@@ -7,22 +7,76 @@
 
 	HOME = $FC58
 	COUT = $FDED
+	PRBYTE = $FDDA
 
 main:
 	jsr HOME
 	jsr print
-	!text "Apple II audit",$8D,0
+	!text "APPLE II AUDIT",$8D,0
 	jsr print
-	!text "Detecting machine version...",$8D,0
+	!text "DETECTING MACHINE VERSION...",$8D,0
+
+	!zone detect {
+	jsr IDENTIFY
+	lda MACHINE
+	bne .known
+	;; MACHINE=0 - unknown machine
+	jsr print
+	!text "UNABLE TO INDENTIFY",$8D,0
+	jmp end
+.known
+	cmp #IIeCard
+	bcc .leiic
+	bne .gs
+;IIeCard
+	jsr print
+	!text "IIE EMULATION CARD",0
+	beq .notsupported
+.gs	;PLUGH
+	jsr print
+	!text "IIGS",0
+.notsupported
+	jsr print
+	!text " NOT SUPPORTED",$8D,0
+	jmp end
+.leiic
+	cmp #IIe
+	bcc .leiii
+	beq .iie
+;IIc
+	jsr print
+	!text "IIC",0
+	beq .notsupported
+.iie
+	jsr print
+	!text "APPLE IIE",$8D,0
+	beq .done
+.leiii
+	cmp #IIplus
+	bcc .iiplain
+	beq .iiplus
+;iiiem
+	jsr print
+	!text "APPLE III IN EMULATION MODE",0
+	beq .notsupported
+.iiplain
+	jsr print
+	!text "PLAIN APPLE II",$8D,0
+	beq .done
+.iiplus	
+	jsr print
+	!text "APPLE II PLUS",$8D,0
+.done
+	} ;detect
+	
 end:	jmp *
 
 ;;; print prints a null-terminated string from the address after the
 ;;; JSR that called it, returning to the address following the null.
 print:
-	tsx
-	lda $101,X
+	pla
 	sta getch+1
-	lda $102,X
+	pla
 	sta getch+2
 -	inc getch+1
 	bne getch
@@ -31,8 +85,11 @@ getch	lda $FEED 		; $FEED gets modified
 	beq +
 	jsr COUT
 	jmp -
-+	lda getch+1
-	sta $101,X
-	lda getch+2
-	sta $102,x
++	lda getch+2
+	pha
+	lda getch+1
+	pha
+	lda #0			; so we can always beq after print-ing
 	rts
+
+	!src "technote2.asm"
